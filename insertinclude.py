5c843f69a808a33f0b637974247777a7545adb21
Yuya Unno <unnonouno@gmail.com>
2013-01-30 14:19:09 +0900

    Do not write blank line when a blank line already exists.

--- insertinclude.py
+++ insertinclude.py
@@ -4,41 +4,55 @@ import os
 import sys
 
 def usage():
-    print "insertinclude.py PROJECT FILE"
+    print "insertinclude.py PROJECT FILE1 [FILE2] .."
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     usage()
     sys.exit(1)
 
 project = sys.argv[1]
-file = sys.argv[2]
+files = sys.argv[2:]
 
-path = os.path.abspath(file)
-dirs = path.split('/')
+def insert_include(project, file):
+    path = os.path.abspath(file)
+    dirs = path.split('/')
 
-if 'src' in dirs:
-    begin = dirs.index('src') + 1
-else:
-    begin = 0
-var = (project + '_' + '_'.join(dirs[begin:])).upper().replace('.', '_') + '_'
+    if 'src' in dirs:
+        begin = dirs.index('src') + 1
+    else:
+        begin = 0
+    var = (project + '_' + '_'.join(dirs[begin:])).upper().replace('.', '_') + '_'
 
-lines = []
-with open(file) as f:
-    for line in f:
-        lines.append(line)
+    lines = []
+    with open(file) as f:
+        for line in f:
+            lines.append(line)
 
-inserted = False
-with open(file, 'w') as f:
-    for l in lines:
-        if "#pragma once" in l:
-            f.write('#ifndef %s\n' % var)
-            f.write('#define %s\n' % var)
-            f.write('\n')
-            inserted = True
-        else:
-            f.write(l)
+    inserted = False
+    prev_ifdef = False
+    prev_blank_line = False
+    with open(file, 'w') as f:
+        for l in lines:
+            blank = (l.strip() == '')
+            if not blank and prev_ifdef:
+                f.write('\n')
 
-    if inserted:
-        f.write('\n')
-        f.write('#endif  // %s\n' % var)
+            if "#pragma once" in l:
+                f.write('#ifndef %s\n' % var)
+                f.write('#define %s\n' % var)
+                prev_ifdef = True
+                inserted = True
+            else:
+                prev_ifdef = False
+                f.write(l)
+            prev_blank_line = blank
 
+        if inserted:
+            if not prev_blank_line:
+                f.write('\n')
+            f.write('#endif  // %s\n' % var)
+
+
+
+for file in files:
+    insert_include(project, file)
